35409459bbc9289f9d23207078998a04bbccec90
Duncan MacIntyre <duncanmacintyre@users.noreply.github.com>
2021-07-20 19:46:33 -0400

    Add script to plot 2D histograms of stellar mass locations

--- analysis/common.py
+++ analysis/common.py
@@ -1,7 +1,18 @@
 # common functions used in multiple other files
 # DM: initial version July 2021
 
+import copy
+
 import numpy as np
+from matplotlib.cm import get_cmap
+from matplotlib.colors import LogNorm
+
+
+# --- code_time_to_Myr
+# given time in code units, return time in Myr
+def code_time_to_Myr(t):
+    return 978.028*t
+
 
 # --- locate_peak_density_3D
 # given an Nx3 array a of coordinates, return coordinates of the location with greatest density
@@ -65,4 +76,84 @@ def locate_peak_density_3D(a, cube_radius, nbins, weights=None, return_histogram
         return (coordinates_of_maximum, edges, d_edges, unique, counts)
     else:
         return coordinates_of_maximum
+
+
+# --- locate_peak_density_3D_and_plot
+# same as locate_peak_density_3D, but also plot 2D histogram(s) showing the result
+#   axes            an Axes to plot on or an iterable of Axes
+#   squish_along    0, 1, or 2 to plot looking along x, y, or z direction; or iterable of these
+#   rasterized      True or False; whether plotted histogram is raster image instead of vector
+#   mark_maximum    True or False; whether to plot a marker showing location of peak density
+#   n_ticks         how many tick marks to use along each axis
+#
+# Others as for locate_peak_density_3D. axes and squish_along should be same length if iterables.
+#
+def locate_peak_density_3D_and_plot(a, cube_radius, nbins, axes,
+                                    squish_along=2, rasterized=True, mark_maximum=True, n_ticks=7,
+                                    weights=None, return_histogram=False):
+    # compute the histogram, find the peak
+    hist_results = locate_peak_density_3D(a, cube_radius, nbins,
+                                          weights=weights, return_histogram=True)
+
+    # plot the 2D histogram(s)
+    # is axes an Axes or an iterable?
+    if hasattr(axes, '__iter__'):
+        # case: iterable
+        for a, sa in zip(axes, squish_along):
+            _plot_results_of_locate_peak_density_3D(
+                a, sa, hist_results, rasterized, mark_maximum, n_ticks)
+    else:
+        # case: just an Axes
+        _plot_results_of_locate_peak_density_3D(
+            axes, squish_along, hist_results, rasterized, mark_maximum, n_ticks)
+
+    # return same output as locate_peak_density_3D would
+    if return_histogram:
+        return hist_results
+    else:
+        return hist_results[0]
+    
+# given output from locate_peak_density_3D, plot a histogram onto ax
+#   ax is an Axes on which to plot
+#   squish_along is 0, 1, or 2
+#   hist_results is a tuple of output from locate_peak_density_3D for return_histogram=True
+#   rasterized is True or False; whether plotted histogram is raster image instead of vector
+#   mark_maximum is True or False; whether to indicate the peak weight density with a marker
+#   n_ticks is how many tick marks to use along each axis
+# doesn't return anything
+cmap = copy.copy(get_cmap('plasma')) # colormap to use
+cmap.set_bad(color = 'w') # changes cmap to use white background
+def _plot_results_of_locate_peak_density_3D(ax, squish_along, hist_results,
+                                            rasterized, mark_maximum, n_ticks):
+    # extract histogram output into variables
+    coordinates_of_maximum, edges, _, unique, counts = hist_results
+    nbins = len(edges) - 1 # how many bins along each direction
+
+    # get indices for the directions that are not squished
+    keep_axis = tuple(i for i in range(3) if i != squish_along)
+
+    # squish 3D histogram along the given direction to get 2D histogram
+    hist2d = np.fromiter(
+        (np.sum(counts[(unique[:,keep_axis[0]]==xi)
+                & (unique[:,keep_axis[1]]==yi)])
+         for yi in range(nbins) for xi in range(nbins)),
+        dtype=counts.dtype,
+        count=nbins**2
+    ).reshape((nbins, nbins))
+    # rows: bins along second kept axis (becomes y-axis on plot)
+    # cols: bins along first kept axis (becomes x-axis on plot)
+
+    # plot the histogram
+    ax.set_xlim(edges[[0, -1]])
+    ax.set_ylim(edges[[0, -1]])
+    ax.set_aspect('equal')
+    ax.pcolormesh(edges, edges, hist2d, norm=LogNorm(), cmap=cmap, rasterized=rasterized)
+    #ax.imshow(hist2d, extent=edges[[0, -1, 0, -1]], origin='lower', norm=LogNorm(), cmap=cmap)
+    # ^- imshow is an alternate raster plotter, but it requires a high figure DPI to have been set
+    if mark_maximum:
+        ax.scatter(coordinates_of_maximum[keep_axis[0]], coordinates_of_maximum[keep_axis[1]],
+                   c='chartreuse', s=10, linewidths=0.5, marker='+')
+    ticks = np.linspace(edges[0], edges[-1], n_ticks)
+    ax.set_xticks(ticks)
+    ax.set_yticks(ticks)
 
---
+++ analysis/plot_snapshot_stellar_mass_histograms.py
@@ -0,0 +1,108 @@
+# plot_snapshot_stellar_mass_histograms.py
+# DM: initial version June 2021, plotting part based on Jupyter Notebook by Doug Rennehan
+#
+#   given GIZMO snapshot files, generate a PDF for each that shows a 2D histogram of the star mass distribution
+#
+#   for usage run `$ python plot_snapshot_stellar_mass_histograms.py --help`
+#   
+
+import multiprocessing
+import sys
+import os
+from argparse import ArgumentParser
+
+import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
+import numpy as np
+import h5py
+
+from common import locate_peak_density_3D_and_plot, code_time_to_Myr
+
+parser = ArgumentParser(
+    description='Given GIZMO snapshot files, generates a PDF for each that shows a 2D histogram of the stellar mass distribution.',
+    epilog='The plot for /path/to/some_snap.hdf5 will be saved at out_dir/some_snap.pdf. Required modules: hdf5, scipy-stack.'
+)
+parser.add_argument('out_dir', help='path to directory in which to save the plots; will be created if it does not already exist')
+parser.add_argument('snaps', nargs='+', metavar='snap.hdf5', help='paths to GIZMO snapshot files to plot')
+parser.add_argument('--squish-along', nargs='+', dest='squish_along', choices=['x', 'y', 'z'], default=('z',), help='which axis to flatten along when making 2D histograms – use more than one to get multiple subplots; default z')
+parser.add_argument('--title', help='title for plots (same for all plots); if not specified, uses snapshot times for titles')
+args = vars(parser.parse_args())
+
+# directory in which to store PDFs
+out_dir = args['out_dir']
+# paths to snapshot files
+snaps = args['snaps']
+# directions to squish along, length gives number of subplots
+squish_along = args['squish_along']
+# title to use in plots, or None to generate titles from times
+title = args['title']
+
+# maps 'x', 'y', 'z' to 0, 1, 2
+map_xyz_012 = {'x':0, 'y':1, 'z':2}
+# maps name of squished direction to names of directions not squished 
+map_not_squished = {'x':('y','z'), 'y':('x','z'), 'z':('x','y')}
+
+# ------ functions
+
+# given a file name for a snapshot, save plot to out_dir
+# most of this function's code is by Doug Rennehan
+def plot_and_save_from_snapshot_file(fname):
+
+    # load the data from the HDF5 snapshot file
+    with h5py.File(fname) as f:
+        try:
+            coords1 = np.asanyarray(f['/PartType4/Coordinates'])
+            masses1 = np.asanyarray(f['/PartType4/Masses'])
+        except KeyError:
+            coords1 = np.empty((0, 3))
+            masses1 = np.empty((0,))
+        coords2 = np.asanyarray(f['/PartType2/Coordinates'])
+        masses2 = np.asanyarray(f['/PartType2/Masses'])
+        time = code_time_to_Myr(f['/Header'].attrs['Time']) # time in Myr
+
+    coords = np.concatenate((coords1, coords2), axis=0)
+    masses = np.concatenate((masses1, masses2), axis=0)
+    
+    # set up a figure and axes
+    fig, ax = plt.subplots(1, len(squish_along),
+                           figsize=(4*len(squish_along), 3.5),
+                           constrained_layout=True)
+
+    # convert ax to an iterable in the case of one subplot
+    if not hasattr(ax, '__iter__'):
+        ax = (ax,)
+
+    # plot histogram onto the axes
+    locate_peak_density_3D_and_plot(coords, axes=ax, cube_radius=75, nbins=512, weights=masses,
+                                    squish_along=tuple(map_xyz_012[sa] for sa in squish_along))
+
+    # set axis labels
+    for a, sa in zip(ax, squish_along):
+        axes_names = map_not_squished[sa]
+        a.set_xlabel('{} [kpc]'.format(axes_names[0]))
+        a.set_ylabel('{} [kpc]'.format(axes_names[1]))
+
+    # set figure title
+    fig.suptitle('t = {:.1f} Myr'.format(time) if title is None else title)
+
+    # save PDF of figure to disk
+    fig.savefig(os.path.join(out_dir, os.path.splitext(os.path.basename(fname))[0] + '.pdf'))
+
+    # delete figure data from memory to free up space
+    fig.clear()
+    plt.close(fig)
+
+    print('Finished {}'.format(fname))
+
+# ----- beginning of main script
+
+# create the output directory if it does not already exist
+try:
+    os.mkdir(out_dir)
+except FileExistsError:
+    pass
+
+# iterate over all of the snapshots, plot and save each
+ncpus = int(os.environ.get('SLURM_CPUS_PER_TASK', default=1))
+with multiprocessing.Pool(processes=ncpus) as pool:
+    pool.map(plot_and_save_from_snapshot_file, snaps, chunksize=1)
