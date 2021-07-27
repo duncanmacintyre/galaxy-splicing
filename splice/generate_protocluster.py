duncanmacintyre/galaxy-splicing

commit 8f75da7a20bc1e4c1b24b9a28814e3370927b790

Keep formed star particles when splicing
This commit makes several changes to generate_protocluster.py:
* Enables the do_star option to keep formed star particles.
* Adds time of star formation to the star fields, ensuring that we keep info about when the stars formed.
* Adds code to change times stars formed to negative times, since the simulation will start at t=0.
* Removes an obsolete comment.

splice/generate_protocluster.py
@@ -28,7 +28,7 @@ import numpy as np
 
 double_precision = True  # use True if GIZMO expects double precision ICs, False if single precision
 do_bulge = False         # whether to include bulge particles in output (we always include disk particles)
-do_star = False          # whether to include formed star particles in output (we always include disk particles)
+do_star = True           # whether to include formed star particles in output (we always include disk particles)
 density_present = False  # whether to include the gas field Density in output
 num_metals = 11          # how many metals we track - set to 0 to ignore Metallicity fields
 r_std = 59.449           # standard deviation for galaxy distance from centre, in kpc - defines normal distribution
@@ -98,6 +98,8 @@ dtype = 'float64' if double_precision else 'float32'
 common_fields = ['Coordinates', 'Masses', 'Velocities']
 # fields for gas particles
 gas_only_fields = ['Density', 'InternalEnergy'] if density_present else ['InternalEnergy']
+# fields for star particles
+star_only_fields = ['StellarFormationTime']
 # fields for gas, star, disk, and bulge particles
 gas_stars_only_fields = ['Metallicity'] if num_metals > 1 else []
 
@@ -115,7 +117,7 @@ gas_fields = (*common_fields, *gas_only_fields, *gas_stars_only_fields, )
 dm_fields = (*common_fields, )
 disk_fields = (*common_fields, )
 bulge_fields = (*common_fields, )
-star_fields = (*common_fields, *gas_stars_only_fields, )
+star_fields = (*common_fields, *star_only_fields, *gas_stars_only_fields, )
 bh_fields = (*common_fields, )
 
 
@@ -281,6 +283,12 @@ for index, data_file in enumerate(fnames_in):
         this_star = {field:grab_property(f, 4, field) for field in star_fields} if do_star else empty_data_star
         this_bh = {field:grab_property(f, 5, field) for field in bh_fields}
 
+    # If we are using data for formed stars, we change their formation times
+    # to negative times since the simulation will start at t=0.
+    if do_star:
+        this_star['StellarFormationTime'] = (this_star['StellarFormationTime']
+                                             - 1.05 * np.max(this_star['StellarFormationTime']))
+
     Ngas[index] = len(this_gas['Coordinates'])
     Ndm[index] = len(this_dm['Coordinates'])
     Ndisk[index] = len(this_disk['Coordinates'])
@@ -432,7 +440,6 @@ new_ids = np.arange(1, tot_part + 1, 1, dtype='uint32')
 print('Generating HDF5 file.')
 with h5py.File(fname_out, 'w') as fp:
 
-    # !!! verify which properties we need - see manual
     # set header properties
     header = fp.create_group('Header')
     header.attrs['MassTable'] = np.zeros(6);

commit f9d71b62e949cb21575b5ebbfd30733428a5a470

Fix bugs in generate_protocluster that were introduced in 8f75da7

splice/generate_protocluster.py
@@ -105,12 +105,13 @@ gas_stars_only_fields = ['Metallicity'] if num_metals > 1 else []
 
 # how many items per field
 field_size = {
-    'Coordinates':     3,
-    'Masses':          1,
-    'Velocities':      3,
-    'Density':         1,
-    'InternalEnergy':  1,
-    'Metallicity':     num_metals
+    'Coordinates':              3,
+    'Masses':                   1,
+    'Velocities':               3,
+    'Density':                  1,
+    'InternalEnergy':           1,
+    'StellarFormationTime':     1,
+    'Metallicity':              num_metals
 }
 
 gas_fields = (*common_fields, *gas_only_fields, *gas_stars_only_fields, )
@@ -206,6 +207,7 @@ def empty_array(field):
     n = field_size[field] # how many entries per particle
     return np.empty((0,) if n==1 else (0, n))
 
+
 # return how many files inside a given directory have names ending with suffix
 def count_files_inside_with_suffix(path, suffix):
     return sum(1 for _ in filter(lambda s: s.endswith(suffix), os.listdir(path)))
@@ -285,7 +287,7 @@ for index, data_file in enumerate(fnames_in):
 
     # If we are using data for formed stars, we change their formation times
     # to negative times since the simulation will start at t=0.
-    if do_star:
+    if do_star and len(this_star['StellarFormationTime']) > 0:
         this_star['StellarFormationTime'] = (this_star['StellarFormationTime']
                                              - 1.05 * np.max(this_star['StellarFormationTime']))
 