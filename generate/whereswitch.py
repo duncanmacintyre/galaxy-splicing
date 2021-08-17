854008d14a6542e8e3f76433037420d7e1f4c24b
Duncan MacIntyre <duncanmacintyre@users.noreply.github.com>
2021-08-16 17:01:35 -0700

    Small code-readability improvements to whereswitch function

--- generate/whereswitch.py
+++ generate/whereswitch.py
@@ -1,7 +1,8 @@
 import numpy as np
 
 # like numpy.where() but can take multiple conditions
-# whereswitch(cond1, x1, [cond2, x2, [cond3, x3, ...]][, y], force_object=False)
+#
+#       whereswitch(cond1, x1, [cond2, x2, [cond3, x3, ...]][, y], force_object=False)
 #
 # returns array with values from x1 where cond1, x2 where cond2 but not cond1,
 # x3 where cond3 but neither cond1 or cond2, and so on
@@ -12,12 +13,12 @@ import numpy as np
 # set force_object=True to ensure that the output has dtype object; this is useful when dealing with
 # strings to make sure that numpy.nan does not get cast as a string
 #
-# pass alternating conditions and array-like
-# if an even number of arguments are given, values where no condition is true are filled with numpy.NaN
-# if an odd number of arguments are given, values where no condition is true are filled with the last argument
+# whereswitch(cond1, x1, [cond2, x2, ...], y)
+#   is equivalent to
+# np.where(cond1, x1, np.where(cond2, x2, np.where(..., y)))
 #
 # e.g.
-#   import numpy as np
+#   >>> import numpy as np
 #   >>> x = np.array([1, 2, 3, 4, 5])
 #   >>> y = np.array([6, 7, 8, 9, 10])
 #   >>> whereswitch(x < 3, y)
@@ -36,20 +37,22 @@ import numpy as np
 
 def whereswitch(*args, force_object=False):
 
-    assert(len(args) > 1) # we require at least two arguments
-
+    assert len(args) > 1 # we require at least two arguments
     # if force_object, convert all even-numbered inputs to type object first
     if force_object:
-        args = [np.array(a, dtype='object') if n%2 != 0 else a for n, a in enumerate(args)]
+        args = [np.asarray(a, dtype='object') if n%2 != 0 else a for n, a in enumerate(args)]
+    return _whereswitch(args)
 
+# helper function for recursion
+def _whereswitch(args):
     if len(args) == 2: # base case: two arguments - call np.where with np.nan as value to fill in gaps
-        return(np.where(args[0], args[1], np.nan))
+        return(np.where(*args, np.nan))
 
     elif len(args) == 3: # base case: three arguments - mimic np.where behaviour
         return(np.where(*args))
 
     else: # recursion case: four or more arguments
-        return(np.where(args[0], args[1], whereswitch(*args[2:])))
+        return np.where(args[0], args[1], _whereswitch(args[2:]))
 
 #   >>> a, b, c = multiwhereswitch(x < 3, ['hello', 'goodbye', np.nan],
 #                                  x < 5, [y, x, 'wow'],
@@ -62,11 +65,6 @@ def whereswitch(*args, force_object=False):
 #           array([nan, nan, 'wow', 'wow', nan], dtype=object)
 
 def multiwhereswitch(*args, force_object=False):
-
-    # N = len(args[1])
-
-    # if len(force_object) == 0:
-    #     force_object = [False for k in range(n)]
 
     return(*[whereswitch(*[a[k] if m%2 != 0 else a for m, a in enumerate(args)],
                          force_object=force_object if type(force_object) == type(True) else force_object[k])
