61735ff80102202a1a76a49e0674048abb0def22
Duncan MacIntyre <duncanmacintyre@users.noreply.github.com>
2021-08-20 18:37:09 -0700

    Add script to easily re-splice realizations from new ICs for the individual galaxies

---
+++ splice/chosen-1e7/resplice.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# The purpose of this script is to re-generate the initial conditions in the 
+# real*/ directories from the HDF5 files for each galaxy. If we change the
+# mass budget and replace the HDF5 file for each galaxy in this directory,
+# then we can run this script to re-splice initial conditions while keeping the
+# same initial position and velocity for each galaxy.
+
+#module load hdf5 scipy-stack
+
+for real in real01{0..4}; do
+    echo "Splicing $real"
+    echo "python ../generate_protocluster.py $real.hdf5 --log $real-log --load $real-log --no-copy"
+    python ../generate_protocluster.py $real.hdf5 --log $real-log --load $real-log --no-copy --nodes 4 --tasks-per-node 48
+done
